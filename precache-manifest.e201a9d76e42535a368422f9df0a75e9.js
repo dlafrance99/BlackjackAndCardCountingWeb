578382ad18a3bd21a51fd127a672956f9208e81d
Devon LaFrance <lafrance.devon@gmail.com>
2020-04-06 14:25:39 -0600

    Updates

---
+++ precache-manifest.e201a9d76e42535a368422f9df0a75e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c31b4d8ab92e7d83cdc305c2d612cd2",
+    "url": "/BlackjackAndCardCountingWeb/index.html"
+  },
+  {
+    "revision": "125c2333e40077dd8612",
+    "url": "/BlackjackAndCardCountingWeb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "943abf48156ffa6c401a",
+    "url": "/BlackjackAndCardCountingWeb/static/js/2.b9284f4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BlackjackAndCardCountingWeb/static/js/2.b9284f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125c2333e40077dd8612",
+    "url": "/BlackjackAndCardCountingWeb/static/js/main.bb240cce.chunk.js"
+  },
+  {
+    "revision": "b135e4d16f6e5176956a",
+    "url": "/BlackjackAndCardCountingWeb/static/js/runtime-main.b3f05a1b.js"
+  }
+]);
